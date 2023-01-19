b10b14bd48be5f27fa819504a0b92f3b4e39041a
Bryan Costa <bryancmoraes776@gmail.com>
2023-01-19 18:27:06 -0300

    develop

--- src/server.js
+++ src/server.js
@@ -1,1 +1,10 @@
-import http from 'http'
+import http from 'node:http'
+
+const server = http.createServer((req, res) =>{
+    return res.send('server')
+})
+
+
+server.listen(8080, () => {
+    console.log('inicialized')
+})
